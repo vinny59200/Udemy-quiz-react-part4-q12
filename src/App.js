92a680f4d2f90b42303f4049ccf7ff0d0ed888f4
vvauban <vvauban@gmail.com>
2023-03-05 19:24:47 +0100

    Add backlink

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Huggable from './components/Huggable';
 
+//https://www.udemy.com/course/react-reactjs-questions-and-answers-2023-job-interviews/?referralCode=C52F8C70EFA5555C18AC
 function App() {
   return (
     <div className="App">
